3f15112d8d5991d846a1610c3c2c4d34813091d4
Arda Kilicdagi <ardakilicdagi@gmail.com>
2016-04-11 18:01:29 +0300

    More env updates

--- config/whatthetag.php
+++ config/whatthetag.php
@@ -1,19 +1,19 @@
 <?php
 
 return [
-	
-	'site_name'		=> 'WhatTheTag',
-	
-	//For social share plugin.
-	//This fills the "shared via @whatthetag" part of the tweet
-	'twitter_name'	=> 'whatthetag',
-	
-	//For disqus comments
-	//If comments_enabled is set to true, no comments is enabled
-	'comments_enabled'	=> true,
-	'disqus_identifier'	=> 'xxxxxxxxxx',
-	
-	//How many content will be displayed per page
-	'pagination_count'	=> 12,
+
+    'site_name' => env('WTT_SITE_NAME', 'WhatTheTag'),
+
+    //For social share plugin.
+    //This fills the "shared via @whatthetag" part of the tweet
+    'twitter_name' => env('WTT_TWITTER_USERNAME', 'whatthetag'),
+
+    //For Disqus comments
+    //If comments_enabled is set to true, no comments is enabled
+    'comments_enabled' => env('WTT_COMMENTS_ENABLED', true),
+    'disqus_identifier' => env('WTT_DISQUS_IDENTIFIER', 'xxxxxxxxxx'),
+
+    //How many content will be displayed per page
+    'pagination_count' => env('WTT_PAGINATION_COUNT', 12),
 
 ];
