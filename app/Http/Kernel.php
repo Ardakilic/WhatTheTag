31b93a6875685dc13663297e889167db3b4bba80
Arda Kilicdagi <ardakilicdagi@gmail.com>
2017-11-27 00:22:53 +0300

    Attempt to upgrade to Laravel 5.3 from 5.2. Act 2.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,6 +20,27 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\VerifyCsrfToken::class,
     ];
 
+
+    /**
+     * The application's route middleware groups.
+     *
+     * @var array
+     */
+    protected $middlewareGroups = [
+        'web' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+        'api' => [
+            'throttle:60,1',
+            'bindings',
+        ],
+    ];
+
     /**
      * The application's route middleware.
      *
